41e519865a5ecc8130bed7f220cbd536acdf5840
Enrico Gueli <enrico.gueli@gmail.com>
2016-03-15 23:50:45 +0100

    added directory scanner

---
+++ 3d/scandir.node.js
@@ -0,0 +1,39 @@
+var fs = require('fs');
+var util = require('util');
+
+var root = {
+	size: 0,
+	children: [],
+	parent: null
+};
+
+
+function scanRecursively(path, branch) {
+	console.log(path);
+	var items = fs.readdirSync(path);
+    for (var i=0; i<items.length; i++) {
+    	var item = items[i];
+    	var stat = fs.statSync(path + "/" + item);
+    	var leaf = {};
+    	leaf.name = item;
+    	leaf.parent = branch;
+    	leaf.children = [];
+    	leaf.size = 0;
+    	if (stat.isFile()) {
+    		leaf.size = stat.blocks;
+    		var toAccumulate = leaf.parent;
+    		while (toAccumulate != null) {
+    			toAccumulate.size += stat.blocks;
+    			toAccumulate = toAccumulate.parent;
+    		}
+    	}
+    	if (stat.isDirectory()) {
+    		var subPath = path + "/" + item;
+    		scanRecursively(subPath, leaf);
+    	}
+        branch.children.push(leaf);
+    }
+}
+
+scanRecursively(".", root);
+console.log(util.inspect(root,{depth:null}));
